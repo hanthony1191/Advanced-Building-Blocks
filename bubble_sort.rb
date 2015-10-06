ea46d5ed396387a1b8377bebdc3f8c48994cba51
Anthony G. Harris <hanthony1191@gmail.com>
2015-10-06 19:33:41 -0400

    Added comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,6 @@
 def bubble_sort(array)
 	iterate = false
+	#Status to determine when to rerun the method
 	array[0...-1].each_with_index do|num, index|
 		if array[index] > array[index + 1]
 			array[index + 1], array[index] = array[index], array[index + 1]
@@ -14,6 +15,7 @@ def bubble_sort_by(array, &block)
 	iterate = false
 	array[0...-1].each_with_index do |num, index|
 		if yield(array[index], array[index + 1]) < 0
+		#yield to place the block as the conditional statement for the loop
 			array[index + 1], array[index] = array[index], array[index + 1]
 			iterate = true
 		end
