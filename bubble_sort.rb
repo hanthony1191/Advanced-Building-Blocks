19fca7ae1e49bf40d83c583c57217179b3e81260
Anthony G. Harris <hanthony1191@gmail.com>
2015-10-06 19:31:15 -0400

    Added sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -9,3 +9,15 @@ def bubble_sort(array)
 	bubble_sort(array) if iterate
 	return array
 end
+
+def bubble_sort_by(array, &block)
+	iterate = false
+	array[0...-1].each_with_index do |num, index|
+		if yield(array[index], array[index + 1]) < 0
+			array[index + 1], array[index] = array[index], array[index + 1]
+			iterate = true
+		end
+	end
+	bubble_sort_by(array, &block) if iterate
+	return array
+end
